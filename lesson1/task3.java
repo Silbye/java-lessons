a6ea9e60ad6b180365487caae108e87c5ee3b378
Silbye <silverxlxl9@gmail.com>
2022-11-27 20:27:58 +0300

    added simple calculator

---
+++ lesson1/task3.java
@@ -0,0 +1,72 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class task3 {
+    static Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int num1 = getInt();
+        int num2 = getInt();
+
+        char operation = getOperation();
+
+        int result = calc(num1,num2,operation);
+
+        System.out.println("Result: "+result);
+    }
+
+    public static int getInt(){
+        System.out.println("Enter number:");
+        int num = 0;
+        if(scanner.hasNextInt()){
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Error, please try entering a number again");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    public static char getOperation(){
+        System.out.println("Enter operation (+) (-) (*) (/):");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Error, please try entering operation again");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int num1, int num2, char operation){
+        int result = 0;
+        switch (operation){
+            case '+':
+                result = num1+num2;
+                return result;
+            case '-':
+                result = num1-num2;
+                return result;
+            case '*':
+                result = num1*num2;
+                return result;
+            case '/':
+                if(num2 == 0){
+                    System.out.println("Can't divide by zero!");
+                    break;
+                }
+                else{
+                    result = num1/num2;
+                    return result;
+                }
+            default:
+                System.out.println("Unknown operation, please try again");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+    }
+}
